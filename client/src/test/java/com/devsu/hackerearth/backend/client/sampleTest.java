41d77a9685d3f329b46707bdc92113d568b1700a
cris6h16 <cristianmherrera21@gmail.com>
2025-10-20 16:40:17 -0500

    my first commit

--- client/src/test/java/com/devsu/hackerearth/backend/client/sampleTest.java
+++ client/src/test/java/com/devsu/hackerearth/backend/client/sampleTest.java
@@ -22,104 +22,88 @@ import com.devsu.hackerearth.backend.client.service.ClientService;
 @SpringBootTest
 public class sampleTest {
 
-    private ClientService clientService = mock(ClientService.class);
-    private ClientController clientController = new ClientController(clientService);
-    @Autowired
-    private TestRestTemplate restTemplate;
+        private ClientService clientService = mock(ClientService.class);
+        private ClientController clientController = new ClientController(clientService);
 
-    @Test
-    void createClientTest() {
-        // Arrange
-        ClientDto newClient = new ClientDto(1L, "Dni", "Name", "Password", "Gender", 1, "Address", "9999999999", true);
-        ClientDto createdClient = new ClientDto(1L, "Dni", "Name", "Password", "Gender", 1, "Address", "9999999999",
-                true);
-        when(clientService.create(newClient)).thenReturn(createdClient);
+        @Test
+        void createClientTest() {
+                // Arrange
+                ClientDto newClient = new ClientDto(1L, "Dni", "Name", "Password", "Gender", 1, "Address", "9999999999",
+                                true);
+                ClientDto createdClient = new ClientDto(1L, "Dni", "Name", "Password", "Gender", 1, "Address",
+                                "9999999999",
+                                true);
+                when(clientService.create(newClient)).thenReturn(createdClient);
 
-        // Act
-        ResponseEntity<ClientDto> response = clientController.create(newClient);
+                // Act
+                ResponseEntity<ClientDto> response = clientController.create(newClient);
 
-        // Assert
-        assertEquals(HttpStatus.CREATED, response.getStatusCode());
-        assertEquals(createdClient, response.getBody());
-    }
+                // Assert
+                assertEquals(HttpStatus.CREATED, response.getStatusCode());
+                assertEquals(createdClient, response.getBody());
+        }
 
-    @Test
-    void clientDomainF5_setters_Test() {
-        // Arrange
-        Client client = Client.builder()
-                .id(1L)
-                .name("Name")
-                .dni("00000")
-                .gender("F")
-                .age(20)
-                .address("address name")
-                .phone("000-100")
-                .password("1234")
-                .isActive(true)
-                .build();
+        @Test
+        void clientDomainF5_setters_Test() {
+                // Arrange
+                Client client = Client.builder()
+                                .id(1L)
+                                .name("Name")
+                                .dni("00000")
+                                .gender("F")
+                                .age(20)
+                                .address("address name")
+                                .phone("000-100")
+                                .password("1234")
+                                .isActive(true)
+                                .build();
 
-        // Act
-        client.setName("NewName");
-        client.setActive(false);
+                // Act
+                client.setName("NewName");
+                client.setActive(false);
 
-        // Assert
-        assertEquals(client.getName(), "NewName");
-        assertEquals(client.isActive(), false);
-    }
+                // Assert
+                assertEquals(client.getName(), "NewName");
+                assertEquals(client.isActive(), false);
+        }
 
-    @Test
-    void integrationTestF6_create_Test() {
-        // Arrange
-        ClientDto createClientDto = ClientDto.builder()
-                .id(999L)
-                .name("Cristian Herrera")
-                .dni("1500000000")
-                .gender("M")
-                .age(21)
-                .address("Tena")
-                .phone("+593960279073")
-                .password("admin123")
-                .isActive(true)
-                .build();
+        @Test
+        void integrationTestF6_create_Test() throws Exception {
+                // Arrange
+                ClientDto createClientDto = ClientDto.builder()
+                                .id(999L)
+                                .name("Cristian Herrera")
+                                .dni("1500000000")
+                                .gender("M")
+                                .age(21)
+                                .address("Tena")
+                                .phone("+593960279073")
+                                .password("admin123")
+                                .isActive(true)
+                                .build();
 
-        ClientDto createdClientDto = ClientDto.builder()
-                .id(1L)
-                .name("Hello")
-                .dni("170700000000")
-                .gender("F")
-                .age(100)
-                .address("Quito")
-                .phone("+593000000000")
-                .password("mypassword")
-                .isActive(false)
-                .build();
+                ClientDto createdClientDto = ClientDto.builder()
+                                .id(1L)
+                                .name("Hello")
+                                .dni("170700000000")
+                                .gender("F")
+                                .age(100)
+                                .address("Quito")
+                                .phone("+593000000000")
+                                .password("mypassword")
+                                .isActive(false)
+                                .build();
 
-        when(clientService.create(any(ClientDto.class))).thenReturn(createdClientDto);
+                when(clientService.create(createClientDto)).thenReturn(createdClientDto);
 
-        // Act
-        ResponseEntity<ClientDto> response = restTemplate.postForEntity("/api/clients", createClientDto,
-                ClientDto.class);
+                // Act
+                ResponseEntity<ClientDto> response = clientController.create(createClientDto);
 
-        // Assert http response
-        assertEquals(HttpStatus.CREATED, response.getStatusCode());
-        assertNotNull(response.getHeaders().getLocation());
-        assertEquals(URI.create("/api/clients/1"), response.getHeaders().getLocation());
+                // Assert
+                assertEquals(HttpStatus.CREATED, response.getStatusCode());
+                assertEquals(createdClientDto, response.getBody());
 
-        // Assert the correct obj is being returned
-        ClientDto body = response.getBody();
-        assertNotNull(body);
-        assertEquals(1L, body.getId());
-        assertEquals("Hello", body.getName());
-        assertEquals("170700000000", body.getDni());
-        assertEquals("F", body.getGender());
-        assertEquals(100, body.getAge());
-        assertEquals("Quito", body.getAddress());
-        assertEquals("+593000000000", body.getPhone());
-        assertEquals("mypassword", body.getPassword());
-        assertFalse(body.isActive());
-
-        // test integration with service
-        verify(clientService).create(createClientDto);
-    }
+                verify(clientService).create(createClientDto);
+        }
 
 }
